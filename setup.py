e77a2cae7b9e78f69ad110e5100a9bace7ef5643
Sandeep <sandeepy@ubuntusydl.(none)>
2013-11-12 21:29:05 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(name='dnspy',
+		version='1.0',
+		description='Library for domain name manipulation',
+		author='Sandeep Yadav',
+		author_email='sandeep.yadav@damballa.com',
+		url='https://github.com/sandeepvaday/dnspy',
+		packages=['dnspy'],
+		license='BSD',
+		)
